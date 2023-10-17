031c51b6801b532ab5fa196f26e137f1d996cb09
WQH <879389031@qq.com>
2023-10-17 20:40:51 +0800

    feat: optimize the example of nc layer

--- examples/vite/src/index.ts
+++ examples/vite/src/index.ts
@@ -113,23 +113,8 @@ setTimeout(async () => {
 }, 0);
 
 //NC图层加载测试
-const defaultValueRange = { min: -100, max: 100 };
-const defaultOffset = { lon: 0, lat: 0, lev: 0 };
-// 默认的粒子运行参数
-const defaultParticleSystemOptions = {
-  maxParticles: 500 * 500, // 最大粒子数(会自动取平方数)
-  particleHeight: 1000.0, // 粒子高度
-  fadeOpacity: 0.95, // 拖尾透明度
-  dropRate: 0.003, // 粒子重置率
-  dropRateBump: 0.01, // 随速度增加的粒子重置率百分比，速度越快越密集，
-  // 最终的粒子重置率particleDropRate = dropRate + dropRateBump * speedNorm;
-  speedFactor: 0.5, // 粒子速度
-  lineWidth: 4.0, // 线宽
-  dynamic: true, // 是否动态运行
-};
+
 // 默认的颜色配置
-// colorTalbe默认为白色，传入的数组为``[[r, g , b], [r, g, b], ...]``格式
-// 例：[[234 / 255, 0, 0], [0, 123 / 255, 0]]，对应粒子colour字段值从低到高
 const defaultColorTable = [
   [0.015686, 0.054902, 0.847059],
   [0.12549, 0.313725, 1.0],
@@ -148,16 +133,8 @@ const defaultColorTable = [
   [0.835294, 0.0, 0.0],
   [0.619608, 0.0, 0.0],
 ];
-const defaultColour = 'speed';
 
-const basicRenderOptions: any = {
-  valueRange: defaultValueRange,
-  offset: defaultOffset,
-  userInput: defaultParticleSystemOptions,
-  colorTable: defaultColorTable,
-  colour: defaultColour,
-};
-
+//加载NC文件
 const file: Blob[] | undefined = [];
 const fileInput: any = document.getElementById('fileInput');
 fileInput.onchange = function () {
@@ -165,21 +142,72 @@ fileInput.onchange = function () {
   console.log(file[0]);
 };
 
+//类型为NCLayerItem
+let loadedNCLayer: any = undefined;
+//测试加载功能
 const loadButton = document.getElementById('load');
 loadButton.addEventListener('click', () => {
-  earth.usePlugin(new NCLayerLoader());
-  earth
-    .addLayer({
-      layerName: 'nc-demo',
-      method: 'nc',
-      input: file[0],
-      //renderOptions: basicRenderOptions,
-    })
-    .then((layer) => {
-      layer.render({
-        layer: layer.instance,
-        ...basicRenderOptions,
+  if (file[0] && loadedNCLayer === undefined) {
+    earth.usePlugin(new NCLayerLoader());
+    earth
+      .addLayer({
+        layerName: 'nc-demo',
+        method: 'nc',
+        input: file[0],
+        renderOptions: {},
+      })
+      .then((layer) => {
+        loadedNCLayer = layer;
+        console.log(loadedNCLayer);
       });
-      console.log(layer);
+  }
+  if (!file[0]) {
+    alert('please select an nc file first!');
+  } else console.log('an NC-file reload is happend');
+});
+//测试渲染1（默认渲染）
+const render0Button = document.getElementById('render0');
+render0Button.addEventListener('click', () => {
+  if (loadedNCLayer) {
+    loadedNCLayer.render({
+      layer: loadedNCLayer.instance,
+      colorTable: defaultColorTable,
     });
+  } else alert('please load an nc file first!');
+});
+//测试渲染2
+const render1Button = document.getElementById('render1');
+render1Button.addEventListener('click', () => {
+  if (loadedNCLayer) {
+    loadedNCLayer.render({
+      layer: loadedNCLayer.instance,
+      colorTable: [[0.015686, 0.054902, 0.847059]],
+    });
+  } else alert('please load an nc file first!');
+});
+//测试渲染3
+const render2Button = document.getElementById('render2');
+render2Button.addEventListener('click', () => {
+  if (loadedNCLayer) {
+    loadedNCLayer.render({
+      layer: loadedNCLayer.instance,
+      colorTable: [
+        [1.0, 0.282353, 0.0],
+        [0.619608, 0.0, 0.0],
+        [0.619608, 0.0, 0.0],
+        [1.0, 0.282353, 0.0],
+        [0.619608, 0.0, 0.0],
+        [1.0, 0.282353, 0.0],
+        [0.619608, 0.0, 0.0],
+      ],
+    });
+  } else alert('please load an nc file first!');
+});
+//测试移除图层
+const removeButton = document.getElementById('remove');
+removeButton.addEventListener('click', () => {
+  if (loadedNCLayer) {
+    loadedNCLayer.remove();
+    loadedNCLayer = undefined;
+  } else alert('please load an nc file first!');
 });
