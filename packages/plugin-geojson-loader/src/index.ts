e7f073d07b707112b991cad451432c884e4bf6d1
RogerWQH2023 <rogerzju2021@gmail.com>
2025-07-23 17:12:19 +0800

    feat: export usefull geojson functions

--- packages/plugin-geojson-loader/src/index.ts
+++ packages/plugin-geojson-loader/src/index.ts
@@ -1,5 +1,7 @@
 import "./api";
 
 export * from "./GeoJsonLayerLoader";
+// export usefull functions
+export * from "./func";
 export type { GeoJsonLayerItem } from "./GeoJsonLayerItem";
 export { DefaultGeoJsonRenderOptions } from "./constant";
